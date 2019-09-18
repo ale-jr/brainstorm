eac62d019bf19966b8e4b26050bf2d732687dc0d
= <eu@alejr.com.br>
2019-09-17 23:44:40 -0300

    correção no envio de mensagens

--- randomQuote.js
+++ randomQuote.js
@@ -3,7 +3,8 @@ const fs = require('fs')
 module.exports = randomQuote =  (path) => new Promise((resolve,reject)=>{
   fs.readFile(path,(err,data)=>{
     if(err) return reject(err)
-    const [quote,author] = String(data).split('\n')[Math.floor(Math.random()*rows.length)]
+    const rows = String(data).split('\n')
+    const [quote,author] = rows[Math.floor(Math.random()*rows.length)]
     resolve({quote,author})
   })
 })
